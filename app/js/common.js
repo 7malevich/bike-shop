7253bd20ef9635d6a814cefde70b0629abc59937
Malevich <malevichivanleonodovich@gmail.com>
2020-09-20 19:45:45 +0300

    small changes

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,23 @@
-$(function() {
+$(function () {
+  $(".baner-slider").slick({
+    //  infinite: true,
+    //  speed: 300,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    nextArrow:
+      '<button class="baner-slider__arows baner-slider__arows--left"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.21839 1L1 9L9.21839 17" stroke="black"/></svg></button>',
+    prevArrow:
+      '<button class="baner-slider__arows baner-slider__arows--right"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.21839 1L1 9L9.21839 17" stroke="black"/></svg></button>',
+  });
 
-	// Custom JS
-
+  $(".js-products-slider").slick({
+    slidesToScroll: 1,
+    slidesToShow: 4,
+    dots: true,
+    infinite: false,
+    nextArrow:
+      '<button class="products-slider__arrows--right"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.21839 1L1 9L9.21839 17" stroke="black"/></svg></button>',
+    prevArrow:
+      '<button class="products-slider__arrows--left"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.21839 1L1 9L9.21839 17" stroke="black"/></svg></button>',
+  });
 });
